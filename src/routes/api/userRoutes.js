ebfc5831a6fcfd947dab8a85d036dddfa7d98246
ismaelvr1999 <ismael.villalpando@alumnos.udg.mx>
2024-08-14 19:34:11 -0600

    Websites Accounts services Documentation added and sign up fix

--- src/routes/api/userRoutes.js
+++ src/routes/api/userRoutes.js
@@ -3,9 +3,9 @@ import httpStatus from 'http-status';
 import  userControllers from '../../controllers/userControllers'
 import authMiddleware from '../../middleware/authMiddleware';
 const router = express.Router();
-router.use('/user',authMiddleware.authUser)
+
 router.post('/user/sign-up',userControllers.signUp);
-router.get('/user',userControllers.getUser);
+router.get('/user',authMiddleware.authUser,userControllers.getUser);
 
 
 export default router;
